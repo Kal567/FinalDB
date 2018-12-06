dc65750fb26a32978a3758b76d69bbfe83f105bf
Saul Feliciano <Saul Feliciano@DESKTOP-B3AVE5B>
2018-12-05 20:41:15 -0400

    Ajustar principal

--- visual/Principal.java
+++ visual/Principal.java
@@ -9,6 +9,10 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JButton;
 
 public class Principal extends JFrame {
 
@@ -35,7 +39,7 @@ public class Principal extends JFrame {
 	 */
 	public Principal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1059, 656);
+		setBounds(100, 100, 799, 662);
 		
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
@@ -79,6 +83,76 @@ public class Principal extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(12, 13, 747, 111);
+		panel.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblOrden = new JLabel("0");
+		lblOrden.setFont(new Font("Tahoma", Font.PLAIN, 23));
+		lblOrden.setHorizontalAlignment(SwingConstants.TRAILING);
+		lblOrden.setBounds(679, 13, 56, 28);
+		panel_1.add(lblOrden);
+		
+		JLabel lblTituloTema = new JLabel("Cancion de Ejemplo");
+		lblTituloTema.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		lblTituloTema.setBounds(31, 13, 643, 57);
+		panel_1.add(lblTituloTema);
+		
+		JLabel lblArtistaGrupo = new JLabel("Artista de Ejemplo ft. El otro Artista");
+		lblArtistaGrupo.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblArtistaGrupo.setBounds(60, 67, 349, 31);
+		panel_1.add(lblArtistaGrupo);
+		
+		JButton btnReproducir = new JButton("Reproducir");
+		btnReproducir.setFont(new Font("Tahoma", Font.BOLD, 26));
+		btnReproducir.setBounds(267, 226, 209, 111);
+		panel.add(btnReproducir);
+		
+		JLabel lblDuraciontxt = new JLabel("Duracion:");
+		lblDuraciontxt.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		lblDuraciontxt.setBounds(278, 364, 108, 27);
+		panel.add(lblDuraciontxt);
+		
+		JLabel lblDuracion = new JLabel("4:23");
+		lblDuracion.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		lblDuracion.setBounds(394, 364, 67, 27);
+		panel.add(lblDuracion);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBounds(12, 447, 333, 119);
+		panel.add(panel_2);
+		panel_2.setLayout(null);
+		
+		JLabel lblAlbumtxt = new JLabel("Album:");
+		lblAlbumtxt.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblAlbumtxt.setBounds(12, 13, 229, 34);
+		panel_2.add(lblAlbumtxt);
+		
+		JLabel lblAlbum = new JLabel("Ejemplo de Album");
+		lblAlbum.setFont(new Font("Tahoma", Font.PLAIN, 28));
+		lblAlbum.setBounds(12, 47, 309, 46);
+		panel_2.add(lblAlbum);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBounds(426, 447, 333, 119);
+		panel.add(panel_3);
+		panel_3.setLayout(null);
+		
+		JLabel lblGenero = new JLabel("Ejemplo de Genero");
+		lblGenero.setFont(new Font("Tahoma", Font.PLAIN, 28));
+		lblGenero.setBounds(12, 47, 309, 46);
+		panel_3.add(lblGenero);
+		
+		JLabel lblGenerotxt = new JLabel("Genero:");
+		lblGenerotxt.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblGenerotxt.setBounds(12, 13, 229, 34);
+		panel_3.add(lblGenerotxt);
 	}
 
 }
